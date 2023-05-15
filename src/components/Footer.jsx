07ca8913b517072a50c7a54209f694ab6a736318
Hanaty <hanatxy@gmail.com>
2023-05-15 09:07:43 -0300

    Footer Tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>Desenvolvido por Hanaty + Simara Conceição (PrograMaria).</p>
+      <p className={styles.footerText}> Development by Hanaty Barros + Programaria</p>
     </div>
   )
 }
